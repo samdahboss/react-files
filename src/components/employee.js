9b4693052d670472d0f297d26588b71dc13d1585
samdahboss <131814523+samdahboss@users.noreply.github.com>
2024-07-25 22:47:38 +0100

    final

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,50 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import "../css/employee.css";
 import "../bootstrap/css/bootstrap.css";
-import maleImage from '../img/male.png';
-import femaleImage from '../img/female.png';
+import maleImage from "../img/male.png";
+import femaleImage from "../img/female.png";
 
-function Employee({employeeData,removeEmployee}) {
-    const [selectedTeam, setSelectedTeam] = useState("Team C");
-    
-    const changeTeam = (event) => {
-        setSelectedTeam(event.target.value);
-    };
-    const handleClick = (employeeId) => {
-        removeEmployee(employeeId)
-    }
-    return (
-        <main className="container">
-            <h1 className="text-center text-white">Team Tracking Application</h1>
-            <div className='d-flex justify-content-sm-between' id='nav-content'>
-                <div id="filter" className='w-50'>
-                    <select className="btn" value={selectedTeam} onChange={changeTeam}>
-                        <option>Team A</option>
-                        <option>Team B</option>
-                        <option>Team C</option>
-                        <option>Team D</option>
-                    </select>
-                </div>
-                <button className="btn btn-dark mx-auto ">
-                    <Link className="text-decoration-none" to="/">Back to Home Page</Link>
-                </button>
+function Employee({ employeeData, removeEmployee }) {
+  const [selectedTeam, setSelectedTeam] = useState("Team C");
+
+  const changeTeam = (event) => {
+    setSelectedTeam(event.target.value);
+  };
+  const handleClick = (employeeId) => {
+    removeEmployee(employeeId);
+  };
+  return (
+    <main className="container">
+      <h1 className="text-center text-white">Team Tracking Application</h1>
+      <div className="d-flex justify-content-sm-between" id="nav-content">
+        <div id="filter" className="w-50">
+          <select className="btn" value={selectedTeam} onChange={changeTeam}>
+            <option>Team A</option>
+            <option>Team B</option>
+            <option>Team C</option>
+            <option>Team D</option>
+          </select>
+        </div>
+        <button className="btn btn-dark mx-auto ">
+          <Link className="text-decoration-none" to="/">
+            Back to Home Page
+          </Link>
+        </button>
+      </div>
+      <div id="container">
+        {employeeData
+          .filter((data) => data.team === selectedTeam)
+          .map((data) => (
+            <div key={data.id} id="employee" className="col-md-12">
+              <button
+                className="btn-danger deleteBtn"
+                onClick={() => handleClick(data.id)}
+              >
+                x
+              </button>
+              <img
+                src={data.gender === "Male" ? maleImage : femaleImage}
+                alt=""
+                width="100%"
+                height="250px"
+              />
+              <h1>{data.name}</h1>
+              <h3>{data.career}</h3>
+              <p>{data.team}</p>
             </div>
-            <div id="container">
-                {employeeData
-                    .filter(data => data.team === selectedTeam)
-                    .map(data => (
-                        <div key={data.id} id="employee" className="col-md-12">
-                            <button className='btn-danger deleteBtn' onClick={()=>handleClick(data.id)}>x</button>
-                            <img src={data.gender === "Male" ? maleImage : femaleImage} alt="" width="100%" height="250px" />
-                            <h1>{data.name}</h1>
-                            <h3>{data.career}</h3>
-                            <p>{data.team}</p>
-                        </div>
-                    ))}
-            </div>
-        </main>
-    );
+          ))}
+      </div>
+    </main>
+  );
 }
 
 export default Employee;
--- src/index.js
+++ src/index.js
@@ -1,115 +1,130 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import EmployeePage from './components/employee';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from './components/Home';
-import ManageEmployee from './components/manageEmployee';
-import { useState } from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import EmployeePage from "./components/employee";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ManageEmployee from "./components/manageEmployee";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Index/>
-);
-function Index(){
-  const [employeeData, setEmployeeData] =useState([
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Index />);
+function Index() {
+  const [employeeData, setEmployeeData] = useState([
     {
-        id:"1",
-        name: "Edidiong",
-        career: "UI/UX designer",
-        team: "Team A",
-        gender: "Female",
+      id: "1",
+      name: "Edidiong",
+      career: "UI/UX designer",
+      team: "Team A",
+      gender: "Female",
     },
     {
-        id:"2",
-        name: "Samuel",
-        career: "Frontend Eng.",
-        team: "Team D",
-        gender: "Male",
+      id: "2",
+      name: "Samuel",
+      career: "Frontend Eng.",
+      team: "Team D",
+      gender: "Male",
     },
     {
-        id:"3",
-        name: "Sola",
-        career: "Web Developer",
-        team: "Team B",
-        gender: "Male",
+      id: "3",
+      name: "Sola",
+      career: "Web Developer",
+      team: "Team B",
+      gender: "Male",
     },
     {
-        id:"4",
-        name: "Ebunoluwa",
-        career: "Project Manager",
-        team: "Team B",
-        gender: "Female",
+      id: "4",
+      name: "Ebunoluwa",
+      career: "Project Manager",
+      team: "Team B",
+      gender: "Female",
     },
     {
-        id:"5",
-        name: "Simi",
-        career: "Product Manager",
-        team: "Team A",
-        gender: "Female",
+      id: "5",
+      name: "Simi",
+      career: "Product Manager",
+      team: "Team A",
+      gender: "Female",
     },
     {
-        id:"6",
-        name: "Raphael",
-        career: "Backend Dev.",
-        team: "Team D",
-        gender: "Male",
+      id: "6",
+      name: "Raphael",
+      career: "Backend Dev.",
+      team: "Team D",
+      gender: "Male",
     },
     {
-        id:"7",
-        name: "Daniel",
-        career: "Data Scientist",
-        team: "Team C",
-        gender: "Male",
+      id: "7",
+      name: "Daniel",
+      career: "Data Scientist",
+      team: "Team C",
+      gender: "Male",
     },
     {
-        id:"8",
-        name: "Oore",
-        career: "UI/UX designer",
-        team: "Team A",
-        gender: "Male",
+      id: "8",
+      name: "Oore",
+      career: "UI/UX designer",
+      team: "Team A",
+      gender: "Male",
     },
     {
-        id:"9",
-        name: "John",
-        career: "Product Manager",
-        team: "Team C",
-        gender: "Male",
+      id: "9",
+      name: "John",
+      career: "Product Manager",
+      team: "Team C",
+      gender: "Male",
     },
     {
-        id:"10",
-        name: "James",
-        career: "Backend Engineer",
-        team: "Team B",
-        gender: "Male",
+      id: "10",
+      name: "James",
+      career: "Backend Engineer",
+      team: "Team B",
+      gender: "Male",
     },
     {
-        id:"11",
-        name: "Suad",
-        career: "Intern",
-        team: "Team B",
-        gender: "Female",
+      id: "11",
+      name: "Suad",
+      career: "Intern",
+      team: "Team B",
+      gender: "Female",
     },
-  ])
+  ]);
   function addEmployee(newEmployee) {
     setEmployeeData([...employeeData, newEmployee]);
   }
   function removeEmployee(employeeId) {
-      setEmployeeData(employeeData.filter((item)=>{
-        if(item.id !== employeeId){
+    setEmployeeData(
+      employeeData.filter((item) => {
+        if (item.id !== employeeId) {
           return true;
-        }
-        else{
+        } else {
           return false;
         }
-      }))
+      })
+    );
   }
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/employee' element={<EmployeePage employeeData={employeeData} removeEmployee={removeEmployee}/>}/>
-        <Route path='/manageEmployee' element={<ManageEmployee employeeData={employeeData} addEmployee={addEmployee}/>}/>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/employee"
+          element={
+            <EmployeePage
+              employeeData={employeeData}
+              removeEmployee={removeEmployee}
+            />
+          }
+        />
+        <Route
+          path="/manageEmployee"
+          element={
+            <ManageEmployee
+              employeeData={employeeData}
+              addEmployee={addEmployee}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
